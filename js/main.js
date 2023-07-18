d5502a391b9b1dbfd09ac18aac4fad0c29e59f55
owlstechbd <owlstech.net@gmail.com>
2023-07-18 17:19:39 +0600

    responsive

--- js/main.js
+++ js/main.js
@@ -1,36 +1,44 @@
+$(document).ready(function() {
+  $('.owl-carousel').owlCarousel({
+    items: 3,
+    loop: true,
+    autoplay: true, 
+    autoplayTimeout: 3000, 
+    nav: false,
+    margin: 20,
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 2
+      },
+      768: {
+        items: 1
+      },
+      992: {
+        items: 2.7
+      }
+    }
+  });
+
+  $('.custom-nav .prev').click(function() {
+    $('.owl-carousel').trigger('prev.owl.carousel');
+  });
+
+  $('.custom-nav .next').click(function() {
+    $('.owl-carousel').trigger('next.owl.carousel');
+  });
+});
 
 
-var From1 = document.getElementById("From1");
-var From2 = document.getElementById("From2");
-var From3 = document.getElementById("From3");
-
-var Next1 = document.getElementById("Next1");
-var Next1 = document.getElementById("Next1");
-var Back1 = document.getElementById("Back1");
-var Back1 = document.getElementById("Back1");
-var progress = document.getElementById("progress");
-
-Next1.onclick = function(){
-  From1.style.left = "-450px";
-  From2.style.left = "40px";
-  // progress.style.left = "170px";
+//popup
+function showPopup() {
+  const popup = document.getElementById('popup');
+  popup.style.display = 'block';
 }
-Back1.onclick = function(){
-  From1.style.left = "50px";
-  From2.style.left = "460px";
-  // progress.style.left = "-20px";
-}
-Next2.onclick = function(){
-  From2.style.left = "-460px";
-  From3.style.left = "50px";
-   // progress.style.left = "250px";
-}
-Back2.onclick = function(){
-  From2.style.left = "50px";
-  From3.style.left = "460px";
-  // progress.style.left = "100px";
-}
 
-// animation
-
-  new WOW().init();
+function closePopup() {
+  const popup = document.getElementById('popup');
+  popup.style.display = 'none';
+}
