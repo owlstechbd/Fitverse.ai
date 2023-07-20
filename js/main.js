b7b7c16107fbefd3e260a9554130cee84c9633f2
owlstechbd <owlstech.net@gmail.com>
2023-07-20 11:09:00 +0600

    some changes

--- js/main.js
+++ js/main.js
@@ -41,4 +41,11 @@ function showPopup() {
 function closePopup() {
   const popup = document.getElementById('popup');
   popup.style.display = 'none';
-}
+};
+
+
+const phoneInputField = document.querySelector("#phone");
+   const phoneInput = window.intlTelInput(phoneInputField, {
+     utilsScript:
+       "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+   });
